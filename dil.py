585b4968d33b8e1c02c215ba0c9b29aaff82c41e
HARSHITH KUMAR <victusharsh6@gmail.com>
2025-04-11 09:18:43 +0530

    Update lab5

--- dil.py
+++ dil.py
@@ -12,36 +12,45 @@ if image is None:
 # Create a 3x3 kernel
 kernel = np.ones((3, 3), np.uint8)
 
-# Apply erosion
+# Apply erosion and dilation
 eroded = cv2.erode(image, kernel, iterations=1)
-
-# Subtract eroded from original
-edge_erosion = cv2.subtract(image, eroded)
-
-# Apply dilation
 dilated = cv2.dilate(image, kernel, iterations=1)
 
-# Subtract original from dilated
-edge_dilation = cv2.subtract(dilated, image)
+# Subtractions
+sub_erosion = cv2.subtract(image, eroded)
+sub_dilation = cv2.subtract(dilated, image)
 
-# Plot all images
-plt.figure(figsize=(12, 6))
+# Absolute differences
+abs_erosion = cv2.absdiff(image, eroded)
+abs_dilation = cv2.absdiff(dilated, image)
 
-plt.subplot(1, 3, 1)
+# Plotting
+plt.figure(figsize=(15, 8))
+
+plt.subplot(2, 3, 1)
 plt.title("Original Image")
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 
-plt.subplot(1, 3, 2)
-plt.title("Edge (Erosion Subtracted)")
-plt.imshow(edge_erosion, cmap='gray')
+plt.subplot(2, 3, 2)
+plt.title("Sub: Original - Eroded")
+plt.imshow(sub_erosion, cmap='gray')
 plt.axis('off')
 
-plt.subplot(1, 3, 3)
-plt.title("Edge (Dilation Subtracted)")
-plt.imshow(edge_dilation, cmap='gray')
+plt.subplot(2, 3, 3)
+plt.title("AbsDiff: Original & Eroded")
+plt.imshow(abs_erosion, cmap='gray')
+plt.axis('off')
+
+plt.subplot(2, 3, 5)
+plt.title("Sub: Dilated - Original")
+plt.imshow(sub_dilation, cmap='gray')
+plt.axis('off')
+
+plt.subplot(2, 3, 6)
+plt.title("AbsDiff: Dilated & Original")
+plt.imshow(abs_dilation, cmap='gray')
 plt.axis('off')
 
 plt.tight_layout()
 plt.show()
-
